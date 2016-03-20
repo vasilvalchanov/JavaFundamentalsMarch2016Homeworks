9fc3f41053a2696bacfc40d238dddb49cd4da56c
vasilvalchanov <vaseto_v@abv.bg>
2016-03-20 16:26:20 +0200

    hhhh

---
+++ 02.MethodsHomework/02.LastDigitOfNumber/02.LastDigitOfNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class LastDigitOfNumber
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(GetLastDigitOfNumberAsWord(number));
+    }
+
+    static string GetLastDigitOfNumberAsWord(int number)
+    {
+        int lastDigit = number % 10;
+        string lastDigitAsWord = "";
+
+        switch (lastDigit)
+        {
+            case 0: lastDigitAsWord = "zero";
+                break;
+            case 1: lastDigitAsWord = "one";
+                break;
+            case 2: lastDigitAsWord = "two";
+                break;
+            case 3: lastDigitAsWord = "three";
+                break;
+            case 4: lastDigitAsWord = "four";
+                break;
+            case 5: lastDigitAsWord = "five";
+                break;
+            case 6: lastDigitAsWord = "six";
+                break;
+            case 7: lastDigitAsWord = "seven";
+                break;
+            case 8: lastDigitAsWord = "eight";
+                break;
+            case 9: lastDigitAsWord = "nine";
+                break;
+
+        }
+
+        return lastDigitAsWord;
+    }
+}
+
